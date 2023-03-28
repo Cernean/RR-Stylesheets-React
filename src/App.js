374915a0f5ade0c19f31e483e3bb7b0ebc39b86f
Constantin Cernean <ccernean@yahoo.com>
2023-03-28 18:54:44 -0400

    Ready

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import './App.css';
+import Feed from './Feed';
 
 function App() {
   const styledText = {
     fontSize: "25px",
-    textAlign: "center",
     margin: "40px",
     border: "1px solid green"
   };
+  const s = {
+    'color': 'red'
+  }
   
   return (
     <div className="App">
       <h1 style={styledText}>Hello World</h1>
+      <Feed s={s} />
     </div>
   );
 }
---
+++ src/Feed.js
@@ -0,0 +1,14 @@
+
+
+function Feed (props) {
+    const s = {
+        'color': 'red'
+    }
+    return (
+        <div>
+            <p styles={props.s}> Exapmle of child components</p>
+        </div>
+    )
+}
+
+export default Feed;
