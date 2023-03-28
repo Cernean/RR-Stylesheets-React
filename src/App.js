74a8b34ae615b4a88d83b3b2312b7cf30e1a2162
Constantin Cernean <ccernean@yahoo.com>
2023-03-27 21:31:01 -0400

    working progress

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
 
 function App() {
+  const styledText = {
+    fontSize: "25px",
+    textAlign: "center",
+    margin: "40px",
+    border: "1px solid green"
+  };
+  
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1 style={styledText}>Hello World</h1>
     </div>
   );
 }
 
 export default App;
+
+  
